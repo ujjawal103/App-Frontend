640f4375f160d95ab576022286ce23a0614a55ed
ujjawal103 <ujjawaljaiswal618@gmail.com>
2026-01-17 02:28:14 +0530

    added payement method on bill also

--- src/components/orders/OrderBillModal.jsx
+++ src/components/orders/OrderBillModal.jsx
@@ -350,6 +350,7 @@ const handlePrint = async () => {
           <strong>Customer:</strong> {order.username || "Guest"} <br />
           <strong>Date:</strong> {new Date(order.createdAt).toLocaleString()}
         </p>
+        <strong className="text-xs">Payment Method:</strong> <span className="text-xs">{order.paymentMethod || "N/A"}</span>
         <div className="line my-2 border-b border-dashed border-gray-300"></div>
 
         {/* Items List */}
